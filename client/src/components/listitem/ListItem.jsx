d189a948f52930cffcbde1dec921aa671d121711
Svancik <thesvancik@gmail.com>
2022-07-04 23:17:18 +0200

    displaying movies from api

--- client/src/components/listitem/ListItem.jsx
+++ client/src/components/listitem/ListItem.jsx
@@ -4,13 +4,33 @@ import {
   ThumbDownOutlined,
   ThumbUpAltOutlined,
 } from "@mui/icons-material";
-import { useState } from "react";
+import axios from "axios";
+import { useState, useEffect } from "react";
 import "./listItem.scss";
 
-export default function ListItem({ index }) {
+//Do komponenty nám vstupuje index (umístění v sidebaru) a item, item = id filmu který je uložen v seznamu (lists).
+//Musíme tedy na základě id (=item) tohoto movie získat movie z mongoDb pomocí axios.get("/movies/find") a uložit výsledek do state.
+export default function ListItem({ index, item }) {
   const [isHovered, setIsHovered] = useState(false);
-  const trailer =
-    "https://player.vimeo.com/external/371433846.sd.mp4?s=236da2f3c0fd273d2c6d9a064f3ae35579b2bbdf&profile_id=139&oauth2_token_id=57447761";
+  const [movie, setMovie] = useState({});
+
+  useEffect(() => {
+    const getMovie = async () => {
+      try {
+        const res = await axios.get("movies/find/" + item, {
+          headers: {
+            token:
+              "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYyYmY3NDQwNWU2N2YxNGJjZDQ1ZDBlYiIsImlzQWRtaW4iOnRydWUsImlhdCI6MTY1Njk1OTcwMSwiZXhwIjoxNjU3MzkxNzAxfQ.5Z451LZ2F1t8BIHgl546F-owiEqJOartZFq-tDFzL7c",
+          },
+        });
+        setMovie(res.data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    getMovie();
+  }, [item]);
+
   return (
     <div
       className="listItem"
@@ -19,10 +39,10 @@ export default function ListItem({ index }) {
       onMouseLeave={() => setIsHovered(false)}
     >
       {" "}
-      <img src="https://i.ytimg.com/vi/Eq6EYcpWB_c/maxresdefault.jpg" alt="" />
+      <img src={movie.img} alt="" />
       {isHovered && (
         <>
-          <video src={trailer} autoPlay={true} loop />
+          <video src={movie.trailer} autoPlay={true} loop />
           <div className="itemInfo">
             <div className="icons">
               <PlayArrow className="icon" />
@@ -31,16 +51,12 @@ export default function ListItem({ index }) {
               <ThumbDownOutlined className="icon" />
             </div>
             <div className="itemInfoTop">
-              <span>1 hour 14 mins</span>
-              <span className="limit">+ 16</span>
-              <span>1999</span>
+              <span>{movie.duration}</span>
+              <span className="limit">{movie.limit}</span>
+              <span>{movie.year}</span>
             </div>
-            <div className="desc">
-              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ut
-              soluta at repudiandae numquam perspiciatis. Inventore, molestias
-              mollitia.
-            </div>
-            <div className="genre">Action</div>
+            <div className="desc">{movie.desc}</div>
+            <div className="genre">{movie.genre}</div>
           </div>
         </>
       )}
