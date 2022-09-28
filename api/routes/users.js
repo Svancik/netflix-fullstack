963770d304048f88523f415fc02c8878eb6da5f4
Svancik <thesvancik@gmail.com>
2022-09-28 22:39:51 +0200

    movies fetch

--- api/routes/users.js
+++ api/routes/users.js
@@ -52,8 +52,8 @@ router.get("/", verify, async (req,res) => {
     const query = req.query.new;
     if(req.user.isAdmin){
         try {
-            //_id:-1 nám předá ty nejdřívější data
-            const users = query ? await User.find().sort({_id:-1}).limit(10) : await User.find()
+            //_id:-1 nám předá ty nejdřívější data;
+            const users = query ? await User.find().sort({_id:-1}).limit(5) : await User.find()
             res.status(200).json(users);
         } catch (error) {
             res.status(500).json(error);
